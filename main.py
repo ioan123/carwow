969f40abd35b17e4a53ab2eb161e9db0c1c50191
Ioannis.Nianios <ioan.nianios@gmail.com>
2018-05-28 17:26:04 +0100

    checking the correct sequence of commands, removing exception

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 from parser import file_check
 from bitmap import Bitmap
 from commands import *
-import logging
-logging.basicConfig(level=logging.INFO)
 
 
 def main():
@@ -15,9 +13,6 @@ def main():
             if k == CREATE_BITMAP:
                 bitmap = Bitmap(v[0], v[1])
             if k == SHOW_CONTENTS:
-                if not bitmap:
-                    log.error("Nothing to show.")
-                    exit(1)
                 bitmap.show_contents()
             if k == COLOUR_PIXEL:
                 bitmap.colour_pixels(v[0], v[1], v[2])
@@ -26,12 +21,8 @@ def main():
             if k == HORIZONTAL_SEGMENT:
                 bitmap.draw_horizontal_segment(v[0], v[1], v[2], v[3])
             if k == CLEAR_TABLE:
-                if not bitmap:
-                    log.error("Nothing to clear.")
-                    exit(1)
-                bitmap.clear_table()
+                bitmap.clear()
 
 
 if __name__ == "__main__":
-    log = logging.getLogger(__name__)
     main()
